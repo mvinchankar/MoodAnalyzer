6c69d191b29da6fd53ecffd725b927dd1719f13b
mvinchankar <mvinchankar18@gmail.com>
2019-11-26 10:10:55 +0530

    ADDED test case for sad mood

--- src/test/java/com/bridgelabz/MoodAnalyzerTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -8,7 +8,7 @@ public class MoodAnalyzerTest {
     @Test
     public void givenMessage_WhenSad_Should_Return_Sad() {
         RealMoodAnalyzer moodAnalyzer = new RealMoodAnalyzer();
-        Assert.assertEquals("SAD", moodAnalyzer.analyzeMood("THIS IS SAD MESSAGE"));
+        Assert.assertEquals("SAD", moodAnalyzer.analyzeMood("This is sad message"));
     }
 
     @Test
