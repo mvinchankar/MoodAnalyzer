2633b57e9833b8222633632592baad29f15ea675
mvinchankar <mvinchankar18@gmail.com>
2019-11-26 10:02:20 +0530

    Added moodanalyzer project

---
+++ src/main/java/com/bridgelabz/RealMoodAnalyzer.java
@@ -0,0 +1,9 @@
+package com.bridgelabz;
+
+public class RealMoodAnalyzer {
+
+    public String analyzeMood(String message) {
+        System.out.println("Ability to analyse and respond Happy or Sad Mood");
+        return "SAD";
+    }
+}
---
+++ src/test/java/com/bridgelabz/MoodAnalyzerTest.java
@@ -0,0 +1,19 @@
+package com.bridgelabz;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+
+    @Test
+    public void givenMessage_WhenSad_Should_Return_Sad() {
+        RealMoodAnalyzer moodAnalyzer = new RealMoodAnalyzer();
+        Assert.assertEquals("SAD", moodAnalyzer.analyzeMood("THIS IS SAD MESSAGE"));
+    }
+
+    @Test
+    public void givenMessage_WhenHappy_Should_Return_Happy() {
+        RealMoodAnalyzer moodAnalyzer = new RealMoodAnalyzer();
+        Assert.assertEquals("HAPPY", moodAnalyzer.analyzeMood("THIS IS HAPPY MESSAGE"));
+    }
+}
